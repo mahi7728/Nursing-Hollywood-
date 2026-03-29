f38f3eb2b5efad53ea7f4c1993a34415d53b4cc2
mahi7728 <mahi7728meena@gmail.com>
2026-03-29 17:39:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = 38758234
+api_hash = "a3e2c6c938fecb485a83fd57ef38bd74"
+bot_token = "8541206964:AAFwhvSmpfvM7ntCYifuy2yoAxVfo62EAoE"
+auth_users = [1392259010]
+sudo_users = [1392259010]
